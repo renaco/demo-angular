6ba66608d8dd57f8ce65ddbf043d24aa69a19b27
Renan Romero <renanromero@gmail.com>
2018-01-05 15:01:30 -0500

    wip(spa) update packages and cli

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import { AppModule } from './app/app.module';
 import { APP_ROUTES_PROVIDERS } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, [APP_ROUTES_PROVIDERS]);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
