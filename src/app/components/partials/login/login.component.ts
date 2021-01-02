9e042babb0bb784888a2ebdf628321fa0a5e8859
renaco <renanromero@gmail.com>
2021-01-02 12:55:52 -0500

    build: update dependencies

--- src/app/components/partials/login/login.component.ts
+++ src/app/components/partials/login/login.component.ts
@@ -9,62 +9,45 @@ import { User } from './user.interface';
 })
 
 export class LoginComponent implements OnInit {
-    public myForm: FormGroup;
-    public submitted: boolean;
-    public events: any[] = [];
+  public myForm: FormGroup;
+  public submitted: boolean;
+  public events: any[] = [];
 
-    constructor(private _fb: FormBuilder) { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
-        // the long way
-        // this.myForm = new FormGroup({
-        //     name: new FormControl('', [<any>Validators.required, <any>Validators.minLength(5)]),
-        //     address: new FormGroup({
-        //         address1: new FormControl('', <any>Validators.required),
-        //         postcode: new FormControl('8000')
-        //     })
-        // });
 
-        // the short way
-        this.myForm = this._fb.group({
-            name: ['', [<any>Validators.required, <any>Validators.minLength(6)]],
-            address: this._fb.group({
-                street: ['', <any>Validators.required],
-                // postcode: ['']
-            })
-        });
+    // the short way
+    this.myForm = this._fb.group({
+      name: ['', [<any>Validators.required, <any>Validators.minLength(6)]],
+      address: this._fb.group({
+        street: ['', <any>Validators.required],
+        // postcode: ['']
+      })
+    });
 
-        // subscribe to form changes
-        this.subcribeToFormChanges();
+    // subscribe to form changes
+    this.subcribeToFormChanges();
 
-        // Update single value
-        (<FormControl>this.myForm.controls['name'])
-            .setValue('John', { onlySelf: true });
+    // Update single value
+    (<FormControl>this.myForm.controls['name'])
+      .setValue('John', { onlySelf: true });
 
-        // Update form model
-        // const people = {
-        // 	name: 'Jane',
-        // 	address: {
-        // 		street: 'High street',
-        // 		postcode: '94043'
-        // 	}
-        // };
-
-        // (<FormGroup>this.myForm)
-        //     .setValue(people, { onlySelf: true });
+    // (<FormGroup>this.myForm)
+    //     .setValue(people, { onlySelf: true });
   }
 
   subcribeToFormChanges() {
-      const myFormStatusChanges$ = this.myForm.statusChanges;
-      const myFormValueChanges$ = this.myForm.valueChanges;
+    const myFormStatusChanges$ = this.myForm.statusChanges;
+    const myFormValueChanges$ = this.myForm.valueChanges;
 
-      myFormStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
-      myFormValueChanges$.subscribe(x => this.events.push({ event: 'VALUE_CHANGED', object: x }));
+    myFormStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
+    myFormValueChanges$.subscribe(x => this.events.push({ event: 'VALUE_CHANGED', object: x }));
   }
 
   save(model: User, isValid: boolean) {
-      this.submitted = true;
-      console.log(model, isValid);
+    this.submitted = true;
+    console.log(model, isValid);
   }
 
 }
