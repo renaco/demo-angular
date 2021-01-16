396b5748c71ebb9488d9505a1869454c6bd690b3
Renan Romero <renan.romero@globant.com>
2021-01-16 16:32:51 -0500

    feat: add reactive paradigm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,28 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 
 export class AppComponent {
+  obs: any;
+
+  ngOnInit() {
+    this.obs = Observable.create(function (observer) {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.next(true);
+      setTimeout(() => {
+        observer.next(4);
+        observer.complete();
+      })
+    })
+  }
+
+  rxjsFun() {
+    console.log('just before subscribe');
+    this.obs.subscribe({
+      next: x => console.log('got value' + x, typeof x),
+      error: err => console.error('something wrong ocurred: ' + err),
+      complete: () => console.log('done'),
+    });
+    console.log('Just after subscribe');
+  }
 }
